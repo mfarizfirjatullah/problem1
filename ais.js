f54e620b22d5ebe5def057d0495cf4f47bfc9f16
unknown <farizfirjatullah@gmail.com>
2023-09-29 01:30:19 +0700

    first commit

---
+++ ais.js
@@ -0,0 +1,90 @@
+// let a = [
+//     [1, 2, 3, 4], //index 0
+//     [5, 6], //index 1
+//     [7,8,9]
+// ]
+// let d = 0
+// for (let i = 0; i < a.length; i++) {
+//     for (let c = 0; c < a[i].length; c++){
+//         for (let p = 0; p < a[i][c].length; p++){
+//             d += a[i][c][p]
+//         }
+        
+//     }
+// }
+// console.log(d)
+// // console.log("hello")
+
+// //input 
+// let word = "Hello World!";
+// let a = "";
+// for (i = word.length - 1 ; i >= 0; i--){
+//     a += word[i]
+    
+// }
+// console.log(a)
+
+// //input
+// let members = [
+//     [12001, "Calvin", "Yuda", "Bekasi", "Informatics"],
+//     [12002, "Luthfi", "Dzulfiqor", "Majalengka", "Management"],
+//     [12003, "Imam", "Abullaisi", "Bandung", "Economics"]
+// ];
+
+// for (let a = 0; a < members.length; a++) {
+//         // console.log(members[a][0])
+//         console.log("Student ID: " + members[a][0])
+//         console.log("Full Name: " + members[a][1])
+//         console.log("City: " + members[a][2])
+//         console.log("Major: " + members[a][3])
+
+// }
+
+
+//expected output 
+// Student ID: 12001
+// Full Name: Calvin Yuda
+// City: Bekasi
+// Major: Informatics
+
+//input
+// let nums = [2,3,6,6,5];
+// let a = 0
+// for (let i = 0; i < nums.length; i++) {
+//     // console.log(nums[i])
+//     a += nums[i]
+//     // console.log(a)
+// }
+//  let result = a / nums.length
+//  console.log(result)
+
+// //expected output 
+// 4,4
+
+let nums = [
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]],
+    [
+        [11, 12, 13],
+        [14, 15, 16],
+        [17, 18, 19]],
+    [
+        [21, 22, 23],
+        [24, 25, 26],
+        [27, 28, 29]]
+];
+let ais = 0
+for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i]);
+   for (let a = 0; a < nums[i].length; a++){
+        // console.log(nums[i][a])
+        for (let p = 0; p < nums[i][a].length; p++){
+            // console.log(nums[i][a][p])
+            ais += nums[i][a][p]
+         
+        }
+    }
+}
+console.log(ais)
---
+++ problem-set-1.js
@@ -0,0 +1,65 @@
+//1
+// function cek (val) {
+//     if (val > 90) {
+//     return val = "AA"
+//     } else if (val > 80 && val <= 90) {
+//         return val = "AB"
+//     } else if (val > 70 && val <= 80) {
+//         return val = "BB"
+//     } else if (val > 60 && val <= 70) {
+//         return val = "BC"
+//     } else if (val > 50 && val <= 60) {
+//         return val = "CC"
+//     } else if (val > 40 && val <= 50) {
+//         return val = "CD"
+//     } else if (val > 30 && val <= 40) {
+//         return val = "DD"
+//     } else if (val <= 30) {
+//         return val = "FF"
+//     }
+// } 
+// console.log(cek(25))
+
+// 2
+// let a = ""
+// for (let index = 0; index < 6; index++) {
+//     a += "#"
+//     console.log(a)    
+// }
+
+//3
+// let a = ""
+// for (let index = 1; index < 6; index++) {
+//     a += index
+//     console.log(a)    
+// }
+
+//4 jadiin fizz buzz nya gatau
+// function a (num){
+//     if (num %3 == 0){
+//         return "Fizz"
+//     } else if (num %5 == 0){
+//         return "Buzz"
+//     } else if (num %3 == 0 && num %5 == 0){
+//         return "FizzBuzz"
+//     } else {
+//         return num
+//     }
+// }
+
+// for (let c = 0; c <= 100; c++) {
+// console.log(a(c))
+// }
+
+//5 ini jadi catur sih tapi kayanya ga bener
+const size = 8
+function a (val) {
+    if (val %2 == 0){
+        return " # # # #" 
+    } else if (val %2 != 0) {
+        return "# # # # "
+    } 
+}
+for (let i = 0; i <= size; i++) {
+ console.log(a(i))
+}
